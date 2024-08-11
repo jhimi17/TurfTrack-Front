f0e4bdfa2c28d17e0f17ee5f4084efb8f76b399b
fridaluz22 <fridaluzf14@gmail.com>
2024-08-11 02:11:08 -0500

    Organizando Front

--- back-end/usuarios_api/models.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const { usuarioSchema } = require('./schemas');
-
-const usuarioModel = mongoose.model('usuario', usuarioSchema);
-
-module.exports = {usuarioModel};
